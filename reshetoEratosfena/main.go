a0f99472a0e2fdb35695fafd9fc3e9ad87f28a70
shart <shart0209@gmail.com>
2023-01-07 18:49:56 +0900

    refactoring

--- reshetoEratosfena/main.go
+++ reshetoEratosfena/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func generateSlice(x int) []int {
 	var sl []int
@@ -12,8 +10,8 @@ func generateSlice(x int) []int {
 	return sl
 }
 
-func removeIndex(s []int, index int) []int {
-	return append(s[:index], s[index+1:]...)
+func removeIndex(s *[]int, index int) {
+	*s = append((*s)[:index], (*s)[index+1:]...)
 }
 
 func main() {
@@ -21,13 +19,12 @@ func main() {
 	items := generateSlice(n)
 
 	for i, p := range items {
+		if p*p > n {
+			break
+		}
 		for j := i + 1; j < len(items); j++ {
-			switch {
-			case p*p > n:
-				break
-			case items[j]%p == 0:
-				sl := removeIndex(items, j)
-				items = sl
+			if items[j]%p == 0 {
+				removeIndex(&items, j)
 			}
 		}
 	}
